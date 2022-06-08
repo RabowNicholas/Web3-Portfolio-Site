814a6df7e227cfd54f6f4fbcf4d808bbc3d7e5e3
Nicholas Rabow <nicholasrabow@Nicholass-Mac-Studio.local>
2022-06-08 12:27:37 -0600

    fix security issue

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,16 +1,40 @@
 const Social = () => {
-    return (
-        <div className="social">
-            <h2 className="text-glow">Socials</h2>
+  return (
+    <div className="social">
+      <h2 className="text-glow">Socials</h2>
 
-            <a href="mailto:rabownick123@gmail.com?subject=Portfolio Interest" className='button'>
-                   Email
-                </a>
-            <a href="https://twitter.com/nick_rabow" target="_blank" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/in/nicholas-rabow-344ab8209/" target="_blank" className="button">LinkedIn</a>
-            <a href="https://github.com/RabowNicholas" target="_blank" className="button">GitHub</a>
-        </div>
-    );
-}
+      <a
+        href="mailto:rabownick123@gmail.com?subject=Portfolio Interest"
+        className="button"
+      >
+        Email
+      </a>
+      <a
+        href="https://twitter.com/nick_rabow"
+        target="_blank"
+        rel="noreferrer"
+        className="button"
+      >
+        Twitter
+      </a>
+      <a
+        href="https://www.linkedin.com/in/nicholas-rabow-344ab8209/"
+        target="_blank"
+        rel="noreferrer"
+        className="button"
+      >
+        LinkedIn
+      </a>
+      <a
+        href="https://github.com/RabowNicholas"
+        target="_blank"
+        rel="noreferrer"
+        className="button"
+      >
+        GitHub
+      </a>
+    </div>
+  );
+};
 
 export default Social;
